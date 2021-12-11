1fbc19e021acdac47dc596cce97a092961bff858
Sajid Ansari <sajidansari33272@gmail.com>
2021-12-11 18:45:06 +0545

    Add bottom navigation

--- lib/screens/controllers.dart
+++ lib/screens/controllers.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Controller extends StatelessWidget {
+class Controller extends StatefulWidget {
   const Controller({Key key}) : super(key: key);
 
   @override
+  _ControllerState createState() => _ControllerState();
+}
+
+class _ControllerState extends State<Controller> {
+  bool _isLightOn = false;
+
+  void toogleSwitch(bool value) {
+    setState(() {
+      _isLightOn = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text("Sajid Ansari");
+    return Card(
+      color: Colors.grey.shade200,
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          SizedBox(
+            height: 10,
+          ),
+          Icon(_isLightOn ? Icons.lightbulb : Icons.lightbulb_outline,
+              size: 50,
+              color: _isLightOn ? Colors.yellow : Colors.grey.shade400),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Light",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+          Text("Whitenoise",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey.shade400,
+                  fontWeight: FontWeight.bold)),
+          Switch(
+              value: _isLightOn,
+              activeColor: Colors.yellow,
+              inactiveThumbColor: Colors.grey.shade200,
+              inactiveTrackColor: Colors.grey.shade400,
+              onChanged: toogleSwitch)
+        ]),
+      ),
+    );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,75 +1,114 @@
 import 'package:flutter/material.dart';
 import 'package:iotcontroller/screens/controllers.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _setIndex = 0;
+
+  void onItemTab(int index) {
+    setState(() {
+      _setIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.brown.shade900,
+      appBar: AppBar(
         backgroundColor: Colors.brown.shade900,
-        appBar: AppBar(
-          backgroundColor: Colors.brown.shade900,
-          elevation: 0,
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 25),
-              child: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Welcome Home",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    CircleAvatar(
-                      radius: 30.0,
-                      backgroundColor: Colors.brown.shade800,
-                      child: const Text('SA'),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("Sajid Ansari",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold)),
-                  ],
-                )
-              ]),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20))),
-                height: 50,
-                child: GridView.builder(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                    ),
-                    itemCount: 10,
-                    itemBuilder: (ctx, index) {
-                      return Controller();
-                    }),
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Welcome Home",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.brown.shade800,
+                    child: const Text('SA'),
+                  )
+                ],
               ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Sajid Ansari",
+                    style: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold)),
+              ),
+            ]),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20))),
+              child: GridView.builder(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemCount: 10,
+                  itemBuilder: (ctx, index) {
+                    return Controller();
+                  }),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        backgroundColor: Colors.brown.shade900,
+        currentIndex: _setIndex,
+        selectedItemColor: Colors.white,
+        selectedIconTheme: IconThemeData(size: 40),
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        elevation: 0,
+        unselectedItemColor: Colors.grey.shade400,
+        mouseCursor: SystemMouseCursors.grab,
+        onTap: (value) {
+          onItemTab(value);
+        },
+      ),
+    );
   }
 }
