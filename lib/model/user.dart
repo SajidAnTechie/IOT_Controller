cfb700ac98e53c4e5f3a1560558f20a66c2d980e
Sajid Ansari <sajidansari33272@gmail.com>
2021-12-28 09:17:12 +0545

    omahaHostClub: Add loginResponse data to home screen

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,24 +1,40 @@
 import 'dart:convert';
-import 'package:iotcontroller/model/loginResponseData.dart';
 
 class UserModel {
-  String id;
-  String name;
-  String email;
-  String token;
+  String status;
+  LoginResponseData data;
 
-  UserModel({this.id, this.name, this.email, this.token});
+  UserModel({this.status, this.data});
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     final data = LoginResponseData.fromJson(json['data']);
-    return UserModel(
-        id: data.id, name: data.name, email: data.email, token: data.token);
+    return UserModel(status: json['status'], data: data);
   }
   Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'email': email, 'token': token};
+    return {'status': status, 'data': data.toJson()};
   }
 }
 
 UserModel loginResponseJson(String str) {
   return UserModel.fromJson(jsonDecode(str));
+}
+
+class LoginResponseData {
+  String id;
+  String name;
+  String email;
+  String token;
+
+  LoginResponseData({this.id, this.email, this.name, this.token});
+
+  factory LoginResponseData.fromJson(Map<String, dynamic> json) {
+    return LoginResponseData(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        token: json['token']);
+  }
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'email': email, 'token': token};
+  }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:iotcontroller/model/controller.dart';
+import 'package:iotcontroller/model/user.dart';
 import 'package:iotcontroller/screens/controllers.dart';
+import 'package:iotcontroller/services/shared_cache.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -11,11 +13,16 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _setIndex = 0;
+  UserModel data;
 
   void onItemTab(int index) {
     setState(() {
       _setIndex = index;
     });
+  }
+
+  Future<void> _somethin() async {
+    data = await SharedCache.getLoginDetails(context);
   }
 
   @override
@@ -26,72 +33,80 @@ class _HomeState extends State<Home> {
           backgroundColor: Colors.grey.shade900,
           elevation: 0,
           automaticallyImplyLeading: false),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Welcome Home",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  CircleAvatar(
-                    radius: 30.0,
-                    backgroundColor: Colors.grey.shade800,
-                    child: const Text('SA'),
-                  )
-                ],
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Sajid Ansari",
-                    style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-              ),
-            ]),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade100,
-                  // boxShadow: [
-                  //   BoxShadow(
-                  //     color: Colors.grey.withOpacity(0.5),
-                  //     spreadRadius: 5,
-                  //     blurRadius: 7,
-                  //     offset: Offset(0, 3), // changes position of shadow
-                  //   ),
-                  // ],
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
-              child: GridView.builder(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10,
-                  ),
-                  itemCount: controllerLists.length,
-                  itemBuilder: (ctx, index) {
-                    final controller = controllerLists[index];
-                    return Controller(controller: controller);
-                  }),
-            ),
-          ),
-        ],
-      ),
+      body: FutureBuilder(
+          future: _somethin(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            return snapshot.connectionState == ConnectionState.waiting
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25),
+                        child: Column(children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Welcome Home",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              CircleAvatar(
+                                radius: 30.0,
+                                backgroundColor: Colors.grey.shade800,
+                                child: const Text('SA'),
+                              )
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(data.data.name,
+                                style: TextStyle(
+                                    color: Colors.grey.shade500,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ]),
+                      ),
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade100,
+                              // boxShadow: [
+                              //   BoxShadow(
+                              //     color: Colors.grey.withOpacity(0.5),
+                              //     spreadRadius: 5,
+                              //     blurRadius: 7,
+                              //     offset: Offset(0, 3), // changes position of shadow
+                              //   ),
+                              // ],
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20))),
+                          child: GridView.builder(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 30),
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                mainAxisSpacing: 10,
+                                crossAxisSpacing: 10,
+                              ),
+                              itemCount: controllerLists.length,
+                              itemBuilder: (ctx, index) {
+                                final controller = controllerLists[index];
+                                return Controller(controller: controller);
+                              }),
+                        ),
+                      ),
+                    ],
+                  );
+          }),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
